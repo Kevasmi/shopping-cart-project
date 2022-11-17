f6aa9d13289f22e98f4539be507b3bfb21442947
Richard Lasco <lasco9@gmail.com>
2022-11-17 14:25:00 -0600

    Added routing using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Header from './components/Header';
+import Home from './components/Home';
+import Megacars from './components/Megacars';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <Header></Header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/megacars' element={<Megacars />} />
+          <Route path='/about-us' element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
+  const navStyle = {
+    color: 'white',
+    textDecoration: 'none',
+  };
+
   return (
     <nav>
       <ul className='nav-links'>
-        <li>
-          <h1 id='logo'>
-            <img
-              src='https://listcarbrands.com/wp-content/uploads/2016/02/Koenigsegg-L%D0%BEgo.png'
-              alt='Koenigsegg logo'
-            />
-            Koenigsegg
-          </h1>
-        </li>
-        <li>Megacars</li>
-        <li>About Us</li>
+        <Link style={navStyle} to='/'>
+          <li>
+            <h1 id='logo'>
+              <img
+                src='https://listcarbrands.com/wp-content/uploads/2016/02/Koenigsegg-L%D0%BEgo.png'
+                alt='Koenigsegg logo'
+              />
+              Koenigsegg
+            </h1>
+          </li>
+        </Link>
+        <Link style={navStyle} to='/megacars'>
+          <li>Megacars</li>
+        </Link>
+        <Link style={navStyle} to='/about-us'>
+          <li>About Us</li>
+        </Link>
       </ul>
       <button className='shopping-cart'>
         <i className='fa fa-shopping-cart'></i>
