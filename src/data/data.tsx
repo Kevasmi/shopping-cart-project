46c9dff81d103c50ddc9628435272210d0ec4606
Richard Lasco <lasco9@gmail.com>
2022-11-20 23:42:04 -0600

    Added more content to data object

---
+++ src/data/data.tsx
@@ -0,0 +1,31 @@
+const carData = [
+  {
+    name: 'CC850',
+    description: `The CC850 is the ideal combination of classic design and
+    cutting-edge technology, and is engineered to be the perfect
+    driver’s car.
+    
+    Inspired by the original CC8S, the CC850 features the classic
+    Koenigsegg silhouette, while the interior, and particularly the
+    analog chronocluster, is a work of art. Meanwhile, every
+    innovation under the hood is designed to enhance the driving
+    experience, including the engage shift system (ESS) and
+    light-speed transmission (LST).`,
+    price: 3650000,
+    img: '../imgs/CC850_Car.jpg',
+  },
+  {
+    name: 'Gemera',
+    description: `The Gemera combines the exciting performance
+    characteristics of a mid-engine two-seater megacar with the
+    practicalities of a four-seater car with more luggage space.
+    
+    This ground-breaking combination means that the Koenigsegg
+    megacar experience can now be shared with family and
+    friends.`,
+    price: 1700000,
+    img: '../imgs/Gemera_Car.jpg',
+  },
+];
+
+export default carData;
