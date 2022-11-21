f2ef0b76297823c5db8e992d798bc96e46377974
Richard Lasco <lasco9@gmail.com>
2022-11-21 15:11:42 -0600

    Created card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <li className='card'>
+      <section className='card-content'>
+        <h2 className='card-title'>CC850</h2>
+        <section className='card-body'>
+          <h3 className='card-price'>$2,800,000</h3>
+          <div className='card-interface'>
+            <div className='card-counter-and-buttons'>
+              <button className='subtract-btn'>-</button>
+              <p className='counter'>0</p>
+              <button className='add-btn'>+</button>
+            </div>
+            <button className='add-cart-btn'>Add</button>
+          </div>
+        </section>
+      </section>
+    </li>
+  );
+}
+
+export default Card;
