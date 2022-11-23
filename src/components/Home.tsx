c11a271047e55a3d551c2c83dc0ded3bd8fc7d03
Richard Lasco <lasco9@gmail.com>
2022-11-23 12:36:23 -0600

    Added styles to components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import '../styles/Home.css';
 
 function Home() {
-  return <div></div>;
+  return <div className='home-page'></div>;
 }
 
 export default Home;
--- src/components/Megacars.tsx
+++ src/components/Megacars.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Card from './Card';
 import carData from '../data/data';
+import '../styles/Megacars.css';
 
 function Megacars() {
   const cardArray = carData;
   const cardItems = cardArray.map((car) => {
     return <Card key={car.id} />;
   });
-  return <ul>{cardItems}</ul>;
+  return <ul className='card-list'>{cardItems}</ul>;
 }
 
 export default Megacars;
