7a1715990c9d25349321569cc62913fbe4dd2b7b
Richard Lasco <lasco9@gmail.com>
2022-12-01 14:13:57 -0600

    Fixed react-router error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Megacars from './components/Megacars';
 import About from './components/About';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import './App.css';
 
 const App: FC = () => {
@@ -18,12 +18,14 @@ const App: FC = () => {
         backgroundImage: 'url(' + require(`${background}`) + ')',
       }}
     >
-      <Header setBackground={setBackground}></Header>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/megacars' element={<Megacars />} />
-        <Route path='/about-us' element={<About />} />
-      </Routes>
+      <Router basename='/shopping-cart-project'>
+        <Header setBackground={setBackground}></Header>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/megacars' element={<Megacars />} />
+          <Route path='/about-us' element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
