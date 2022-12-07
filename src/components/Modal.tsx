7755ea959561b4663a89192f82d4343e8284c485
Richard Lasco <lasco9@gmail.com>
2022-12-07 15:44:24 -0600

    Created and appllied stylesheet for Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import { CloseButton } from 'react-bootstrap';
 import CartList from './CartList';
+import '../styles/Modal.css';
 
 interface IProps {
   handleClose: () => void;
@@ -10,18 +12,25 @@ interface IProps {
 
 const ShoppingCart: FC<IProps> = ({ handleClose, show }) => {
   return (
-    <Modal show={show} onHide={handleClose} backdrop='static'>
-      <Modal.Header closeButton>
+    <Modal
+      show={show}
+      onHide={handleClose}
+      backdrop='static'
+      size='lg'
+      className='cart-modal'
+    >
+      <Modal.Header className='cart-modal-close-btn'>
         <Modal.Title>Your Cart:</Modal.Title>
+        <CloseButton onClick={handleClose} variant='white' />
       </Modal.Header>
       <Modal.Body>
         <CartList></CartList>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant='secondary' onClick={handleClose}>
+        <Button variant='light' onClick={handleClose}>
           Close
         </Button>
-        <Button variant='primary'>Understood</Button>
+        <Button variant='primary'>Purchase</Button>
       </Modal.Footer>
     </Modal>
   );
